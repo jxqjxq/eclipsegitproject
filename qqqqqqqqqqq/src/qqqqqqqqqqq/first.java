4cf7d150eede01f92ea69e8de0a1cebc82cdb623
jxqjxq <47745746+jxqjxq@users.noreply.github.com>
2019-02-20 12:59:50 +0800

    Update first.java
    修改版

--- qqqqqqqqqqq/src/qqqqqqqqqqq/first.java
+++ qqqqqqqqqqq/src/qqqqqqqqqqq/first.java
@@ -3,8 +3,8 @@ package qqqqqqqqqqq;
 public class first {
 
 	public static void main(String[] args) {
-			System.out.println("666666");
-			System.out.println("666666");
+			System.out.println("666777777666");
+			System.out.println("66667777766");
 			System.out.println("666666");
 			System.out.println("666666");
 	}
