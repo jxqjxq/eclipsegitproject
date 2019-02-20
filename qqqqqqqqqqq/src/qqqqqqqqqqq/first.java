369be2be40dc4897fea5760f89f27cd6f2448348
jxq <957999216@qq.com>
2019-02-20 13:01:43 +0800

    redis版本

--- qqqqqqqqqqq/src/qqqqqqqqqqq/first.java
+++ qqqqqqqqqqq/src/qqqqqqqqqqq/first.java
@@ -1,13 +1,21 @@
 package qqqqqqqqqqq;
 
+
 public class first {
 
 	public static void main(String[] args) {
+			/*System.out.println("666666");
 			System.out.println("666666");
 			System.out.println("666666");
 			System.out.println("666666");
-			System.out.println("666666");
-			System.out.println("666666");
+			System.out.println("666666");*/
+			
+			Jedis redis =new Jedis("localhost",6379);
+			redis.set("userName", "666666");
+			System.out.println(redis.get("userName"));
+			redis.close();
+			
 	}
 
 }
+ 
---
+++ qqqqqqqqqqq/src/qqqqqqqqqqq/tttttt.java
@@ -0,0 +1,12 @@
+package qqqqqqqqqqq;
+
+public class tttttt {
+
+	public static void main(String[] args) {
+		
+		System.out.println("怎么只提交一个文件");
+		
+
+	}
+
+}
