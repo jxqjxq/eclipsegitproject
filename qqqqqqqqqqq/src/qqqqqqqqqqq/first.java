1e092cc19908ec7833aa59fb414c05c6eb73b36a
jxqjxq <47745746+jxqjxq@users.noreply.github.com>
2019-02-20 20:29:53 +0800

    Update first.java
    tell me why

--- qqqqqqqqqqq/src/qqqqqqqqqqq/first.java
+++ qqqqqqqqqqq/src/qqqqqqqqqqq/first.java
@@ -5,7 +5,7 @@ public class first {
 
 	public static void main(String[] args) {
 		/*	System.out.println("666777777666");
-			System.out.println("66667777766");
+			System.out.println("tell me why");
 			System.out.println("666666");
 			System.out.println("666666");*/
 			
